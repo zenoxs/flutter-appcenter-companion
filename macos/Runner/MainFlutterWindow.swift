51b8fe6966e2424a58e315490d4bea68431e8b0d
Zenoxs <amaury.civier@gmail.com>
2022-05-02 14:28:07 +0200

    better windo manager integration

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,6 +1,7 @@
 import Cocoa
 import FlutterMacOS
 import flutter_acrylic
+import window_manager
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
@@ -15,5 +16,10 @@ class MainFlutterWindow: NSWindow {
     RegisterGeneratedPlugins(registry: blurryContainerViewController.flutterViewController)
 
     super.awakeFromNib()
+  }
+
+  override public func order(_ place: NSWindow.OrderingMode, relativeTo otherWin: Int) {
+    super.order(place, relativeTo: otherWin)
+    hiddenWindowAtLaunch()
   }
 }
